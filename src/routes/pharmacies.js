712d2f9aed7cea4f096f5850b03e764636c1e06c
Atila Guler <128936466+atilagulers@users.noreply.github.com>
2023-10-08 21:29:30 +0300

    Update pharmacies.js

--- src/routes/pharmacies.js
+++ src/routes/pharmacies.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import {getAllPharmacies, getPharmacy} from '../controllers/pharmacies.js';
+import {updatePharmacies} from '../utils/scheduler./js'
 
 const router = express.Router();
 
 router.get('/', getAllPharmacies);
+router.get('/update',  async (req, res) => {
+  try {
+    await updatePharmacies();
+    res.send('pharmacies updated');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while updating pharmacies');
+  }
+})
 router.get('/:id', getPharmacy);
 
 export default router;
+
